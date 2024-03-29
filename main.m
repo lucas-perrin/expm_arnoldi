905f5e460a344906f8e5b2b30a59c443cfa85294
Lucas Perrin <lucas.g.perrin@gmail.com>
2024-03-29 11:52:24 +0100

    change in display and printing speedup

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-size_vec = 15;
+size_vec = 10;
 dim = 3;
 dimensions = size_vec*ones(1,dim);
 
@@ -12,7 +12,7 @@ b = rand(prod(dimensions),1);
 tic
 [Q,h,expAb_anroldi,n_bk] = Arnoldi_exp(A,b,size_vec,1,40);
 time_arnoldi = toc;
-n_bk
+fprintf(['convergence at default toler at n = ',num2str(n_bk),'\n'])
 tic
 expAb_matlab  = expm(A)*b;
 time_matlab = toc;
@@ -20,5 +20,5 @@ time_matlab = toc;
 error = norm(expAb_anroldi - expAb_matlab)/norm(expAb_anroldi);
 
 fprintf('\n')
-fprintf('size \\ times | Arnoldi  |  Matlab   |   Error \n')
-fprintf(['    ',num2str(prod(dimensions),'%6d'),'     | ',num2str(time_arnoldi,'%.6f'),' | ',num2str(time_matlab,'%.6f'),' | ',num2str(error,'%4e'),' \n\n'])
+fprintf('size \\ times | Arnoldi  |  Matlab  |    Error   |  Speedup \n')
+fprintf(['    ',num2str(prod(dimensions),'%6d'),'     | ',num2str(time_arnoldi,'%.6f'),' | ',num2str(time_matlab,'%.6f'),' | ',num2str(error,'%.4e'),' | ',num2str(time_matlab/time_arnoldi*100,'%.4f'),'%% \n\n'])
